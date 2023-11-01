163118ae12a47bc57ae7fcfa4ab7cb98d560aeee
Alejandro <alejandro.bacquet@gmail.com>
2023-11-01 15:13:30 -0300

    Tercer Commit

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from inicio.models import Paleta
+from inicio.models import Pelicula,Serie,Libro
 
-admin.site.register(Paleta)
+admin.site.register(Libro)
+admin.site.register(Pelicula)
+admin.site.register(Serie)
 
 # Register your models here.
