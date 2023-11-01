ee94d157eff9f99c56c408a52537f83c5a75867f
Alejandro <alejandro.bacquet@gmail.com>
2023-11-01 14:54:13 -0300

    Tercer Commit

--- inicio/models.py
+++ inicio/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class Paleta(models.Model):
-    marca = models.CharField(max_length=30)
-    descripcion = models.TextField()
-    anio = models.IntegerField()
-    
-    def __str__(self):
-        return f'{self.id} - {self.marca} - {self.anio}'
-    
 class Pelicula(models.Model):
     nombre_pelicula = models.CharField(max_length=50)
     tipo_pelicula = models.TextField()
